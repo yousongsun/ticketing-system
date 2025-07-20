75a71756b6df9aaba4fc8654d82aea388a78efce
ysun209 <ysun209@aucklanduni.ac.nz>
2025-07-20 18:37:19 +1200

    feat: add QR code generation api endpoint for seat booking

--- packages/backend/src/routes/api/api.ts
+++ packages/backend/src/routes/api/api.ts
@@ -19,6 +19,10 @@ router.use('/seats', seatRoutes);
 import ordersRoutes from './api-orders';
 router.use('/orders', ordersRoutes);
 
+// Import qr code route
+import qrCodeRoutes from './api-qrcode';
+router.use('/qrcode', qrCodeRoutes);
+
 // Import payment route
 import stripeRoutes from './api-stripe';
 router.use('/stripe', stripeRoutes);
